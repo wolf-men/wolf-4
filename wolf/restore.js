764a396f7519808df3878c93abaff43922d2a011
wolf-men <96054799+wolf-men@users.noreply.github.com>
2021-12-13 16:52:13 +0100

    Add files via upload

--- wolf/restore.js
+++ wolf/restore.js
@@ -26,34 +26,34 @@ function consolecheck(){
 		switch (fwVersionp34) {
 			
 			case ps4fwCompat[0]:
-     		   document.location.href="https://wolf-4.github.io/restore/505/index.html"
+     		   document.location.href="https://wolf-5.github.io/restore/505/index.html"
 			    break;
 				
 			case ps4fwCompat[1]:
-			   document.location.href="https://wolf-4.github.io/restore/505/index.html"
+			   document.location.href="https://wolf-5.github.io/restore/505/index.html"
 				break;
 
 			case ps4fwCompat[2]:
-			   document.location.href="https://wolf-4.github.io/restore/672/index.html"
+			   document.location.href="https://wolf-5.github.io/restore/672/index.html"
 				break;				
 			case ps4fwCompat[3]:
-			   document.location.href="https://wolf-4.github.io/restore/702/index.html"
+			   document.location.href="https://wolf-5.github.io/restore/702/index.html"
 				break;				
 
 			case ps4fwCompat[4]:
-			   document.location.href="https://wolf-4.github.io/restore/702/index.html"
+			   document.location.href="https://wolf-5.github.io/restore/702/index.html"
 				break;				
 
 			case ps4fwCompat[5]:
-			   document.location.href="https://wolf-4.github.io/restore/755/index.html"
+			   document.location.href="https://wolf-5.github.io/restore/755/index.html"
 				break;
 				
 			case ps4fwCompat[6]:
-			    document.location.href="https://wolf-4.github.io/restore/755/index.html"
+			    document.location.href="https://wolf-5.github.io/restore/755/index.html"
 				break;
 			
 			case ps4fwCompat[7]:
-			   document.location.href="https://wolf-4.github.io/restore/755/index.html"
+			   document.location.href="https://wolf-5.github.io/restore/755/index.html"
 				break;
 															
 			default:
